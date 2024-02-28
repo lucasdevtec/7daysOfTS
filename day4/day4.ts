d785a61f11a1a60d1ab3f3baf43cac1fb5ad445e
Lucas <lucasprogjr@gmail.com>
2024-02-28 16:12:16 -0300

    day4

---
+++ day4/day4.ts
@@ -0,0 +1,9 @@
+console.log("Changes");
+console.log("Mudou");
+function red(params: string) {
+  if (params) {
+    return params + "red";
+  } else {
+    return;
+  }
+}
---
+++ day4/js/day4.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Changes");
+console.log("Mudou");
+function red(params) {
+    if (params) {
+        return params + "red";
+    }
+    else {
+        return;
+    }
+}
