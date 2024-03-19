0fa25d9262cbeced4acac693a51b47c50a8ab408
Lucas <lucasprogjr@gmail.com>
2024-03-18 21:35:04 -0300

    Day7

---
+++ day7/js/ex3.js
@@ -0,0 +1,23 @@
+"use strict";
+// Day 7 - Exercise 3
+class Furniture {
+    constructor() {
+        this.manufacturer = "IKEA";
+    }
+}
+class Desk extends Furniture {
+    kind() {
+        console.log(`This is a desk made by ${this.manufacturer}`);
+    }
+}
+class Chair extends Furniture {
+    kind() {
+        console.log(`This is a chair made by ${this.manufacturer}`);
+    }
+}
+const desk = new Desk();
+desk.kind();
+//desk.manufacturer; // Deve retornar um erro
+const chair = new Chair();
+chair.kind();
+//chair.manufacturer; // Deve retornar um erro
